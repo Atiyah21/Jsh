1b66a98b5ef808b797160ec2989e8bfd512ff13e
friedman <eliot.friedmann@etu.u_paris.fr>
2023-11-23 16:17:15 +0100

    Nom de varible changé

--- jsh.c
+++ jsh.c
@@ -8,61 +8,62 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-static char **decoupe_chemin(char *chemin, int *x)
+static char **parceur(char *ligne, int *x)
 {
-        assert(chemin != NULL);
-        assert(*chemin != '\0');
+        assert(ligne != NULL);
+        assert(*ligne != '\0');
 
-        size_t len = strlen(chemin);
-        if (*(chemin + len - 1) == ' ')
+        size_t len = strlen(ligne);
+        if (*(ligne + len - 1) == ' ')
                 return NULL;
 
         size_t nb_mots = 1;
 
         for (size_t i = 0; i < len; ++i)
-                if (*(chemin + i) == ' ')
+                if (*(ligne + i) == ' ')
                         ++nb_mots;
         *x = nb_mots;
-        char **chemin_decoupe = malloc(nb_mots * sizeof(char *));
-        *chemin_decoupe = strtok(chemin, " ");
+        char **ligne_decoupe = malloc(nb_mots * sizeof(char *));
+        *ligne_decoupe = strtok(ligne, " ");
 
         for (size_t i = 1; i < nb_mots; ++i)
-                *(chemin_decoupe + i) = strtok(NULL, " ");
+                *(ligne_decoupe + i) = strtok(NULL, " ");
 
-        return chemin_decoupe;
+        return ligne_decoupe;
 }
 
-	 int pwd(){
+int pwd()
+{
 
-                char *actuel = malloc(PATH_MAX);
+        char *actuel = malloc(PATH_MAX);
 
-                if(getcwd(actuel, PATH_MAX) == NULL){
-                        perror("pwd: Erreur getcwd");
-                        free(actuel);
-                        return 1;
-                }
-
-                printf("%s\n", actuel);
+        if (getcwd(actuel, PATH_MAX) == NULL)
+        {
+                perror("pwd: Erreur getcwd");
                 free(actuel);
-                return 0;
+                return 1;
         }
 
+        printf("%s\n", actuel);
+        free(actuel);
+        return 0;
+}
 
 int main(int argc, char const *argv[])
 {
         int x;
         rl_initialize();
-        //rl_outstream = stderr;
+        // rl_outstream = stderr;
         while (1)
         {
                 char *buf = readline(">");
-                char **ligne = decoupe_chemin(buf, &x);
+                char **ligne = parceur(buf, &x);
                 for (int i = 0; i < x; ++i)
                 {
-                        if(strcmp(*(ligne+i), "pwd") == 0)
-				pwd();
-			else
-				printf("%s\n", *(ligne + i));
+                        if (strcmp(*(ligne + i), "pwd") == 0)
+                                pwd();
+                        else
+                                printf("%s\n", *(ligne + i));
                 }
         }
         return 0;
