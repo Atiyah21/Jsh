18c4f863130e5bcb2c16569004f12aa6e639a9a8
Boudelaa <fares.boudelaa@etu.u-paris.fr>
2023-12-21 18:33:26 +0100

    TRIPLE MONSTRE

--- jsh.c
+++ jsh.c
@@ -9,12 +9,20 @@
 
 typedef struct
 {
+  //pid -1 veut dire que c'est vide
   pid_t pid;
   char command[256];
+  /*
+  1 -> Done
+  0 -> en cours
+  -1 -> Killed
+  2 -> Sleeping? 
+  */
   int status;
+  int index;
 } Job;
 
-Job jobs[128];
+Job jobs[512];
 int num_jobs = 0;
 static int ret = 0;
 // char *s = NULL;
@@ -22,15 +30,23 @@ static int ret = 0;
 int pid;
 static char prev_directory[PATH_MAX];
 
-int getNumJobs()
+void update_num_jobs()
 {
   int res = 0;
   for (int i = 0; i < 128; i++)
   {
-    if (jobs[i].status >= 0)
+    if (jobs[i].pid!=-1)
       res++;
   }
-  return res;
+  num_jobs = res;
+}
+
+int get_smallest_index(){
+  for(int i = 0 ; i< 512 ; i++){
+    if (jobs[i].status==-1) return i;
+  }
+  perror("No space left");
+  return -1;
 }
 
 int execute(int argc, char **argv)
@@ -57,6 +73,8 @@ int execute(int argc, char **argv)
     {
 
       jobs[num_jobs].pid = pid;
+      jobs[num_jobs].index = get_smallest_index() ;
+      jobs[num_jobs].status = 1;
       strcpy(jobs[num_jobs].command, argv[0]);
       for (int i = 1; i < argc; i++)
       {
@@ -142,8 +160,34 @@ int execute(int argc, char **argv)
       }
       else
         tmp = ret;
-    }
-    else if (strcmp(argv[0], "jobs") == 0)
+    } else if (strcmp(argv[0], "kill") == 0)
+    {
+    if (argv[1] != NULL && argv[1][0] == '%'){
+
+                        if (strlen(argv[1]) >= 2){
+
+                                int k = atoi(argv[1] + 1) - 1;
+
+                                if (k >= 0 ){
+
+                                        kill(jobs[k].pid, SIGTERM);
+                                        if (jobs[k].status != 1){
+                                                jobs[k].status = -1;
+                                        }
+                                }
+                                else{
+                                        printf("Invalid job index\n");
+                                }
+                        }
+                        else{
+                                printf("Invalid argument for kill\n");
+                        }
+                }
+                else{
+                        printf("Invalid syntax for kill\n");
+                }
+
+    }else if (strcmp(argv[0], "jobs") == 0)
     {
       for (int i = 0; i < num_jobs; i++)
       {
@@ -177,6 +221,12 @@ int execute(int argc, char **argv)
     }
     return tmp;
   }
+}
+
+void empty(int i){
+  jobs[i].status=-1;
+  jobs[i].index=-1;
+  jobs[i].command[0]='\0';
 }
 
 int split(char *str, int *nbw, char **res)
@@ -243,6 +293,24 @@ int main(int argc, char const *argv[])
           close(fd);
         fd = -1;
       }
+    }
+      while (num_jobs > 0){
+      pid_t p = waitpid(-1, NULL, WNOHANG);
+      if (p > 0){
+         for (int i = 0; i < 512; i++) {
+          if (jobs[i].pid == p) {
+            if(jobs[i].status == -1 )
+              fprintf(stderr, "[%d]   %d       Killed  %s\n", i, jobs[i].pid, jobs[i].command);
+            else if(jobs[i].status == 1 )
+              fprintf(stderr, "[%d]   %d       Done    %s\n", i, jobs[i].pid, jobs[i].command);
+            num_jobs--;
+            empty(i);
+            
+          }
+         }
+      }
+      else
+          break;
     }
   }
 
